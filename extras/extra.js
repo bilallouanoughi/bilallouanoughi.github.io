22b6dcdfa59e138058ffb8783a70711b8bd80a12
bilallouanoughi <bilallouanoughi@gmail.com>
2025-05-03 23:21:29 +0200

    Premier commit : ajout du site

---
+++ extras/extra.js
@@ -0,0 +1,148 @@
+<html lang="FR"><head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- titre de la page -->
+    <title>Agenceco</title>
+
+    <!-- icone de l'onglet -->
+     <link rel="stylesheet" href="./png-clipart-round-green-and-white-plant-logo-eve-t-shirt-symbol-pin-badges-wall-e-leaf-logo.png">
+     </head><body><h1>Agenceco</h1>
+     <div class="header">
+        
+        <ul>
+            <li>Accueil</li>
+            <li>Services</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+    
+     
+           <link rel="stylesheet" href="site-ecolo.css">
+
+    <nav>
+<!-- ul = Unorder List  -->
+
+<ul>
+    <li>Accueil</li>
+    <li>Notre equipe</li>
+    <li>Nos services</li>
+    <li>Témoignages</li>
+    <li>Contactez-nous</li>
+    <li>Notre galerie</li>
+</ul>
+
+
+
+    </nav>
+<h2>Bienvenue dans votre Agence web écologique</h2>
+
+<section>
+<img src="./feuilles.jpg" alt="image-selection" height="1440">
+
+</section>
+<h4>Notre équipe</h4>
+
+<strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec sed leo in nisi vehicula bibendum. Aliquam sit amet blandit nisi, sodales molestie.
+</strong> 
+<div class="image-container">
+    <img src="image 1.jpg" alt="Image 1" class="rounded" height="125" width="125">
+    <img src="image 2.jpg" alt="Image 2" class="rounded" height="125" width="125">
+    <img src="image 3.jpg" alt="Image 3" class="rounded" height="125" width="125">
+    <div>
+        
+    </div>
+</div>
+<strong>Julie Dupond</strong>
+<strong>Romeo Sylla</strong>
+<strong>Javier Bardem</strong>
+
+
+<p>2500
+    client satisfaits
+</p>
+<p>2500
+    client satisfaits
+</p>
+<p>2500
+    client satisfaits
+</p>
+<p>2500
+    client satisfaits
+</p>
+<h2>Notre services</h2>
+
+<img src="./Vector.jpg" alt="image-selection">
+<img src="./Vector-1.jpg" alt="image-container">
+
+
+<h2>Contactez-nous</h2>
+<strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit. donec sed leo in.</strong>
+
+
+<section>
+<form action=""></form>
+
+<label for="name">Nom</label>
+<input type="text" id="name">
+
+
+<label for="Catégorie">Catégorie</label>
+<input type="text" name="Catégorie" id="Catégorie">
+
+<label for="Téléphone">Téléphone</label>
+<input type="tel" name="Téléphone" id="Téléphone">
+
+<label for="Ville">Ville</label>
+<input type="text" name="Ville" id="Ville">
+
+<label for="email">email</label>
+<input type="email" name="email" id="email">
+
+
+<label for="Message">Message</label>
+<textarea name="Message" id="Message"></textarea>
+
+
+
+<input type="submit" name="Envoyer" id="Envoyer">
+
+<img src="./jason-goodman-Oalh2MojUuk-unsplash 1.jpg" alt="" height="300" width="200">
+
+
+
+
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body></html>
